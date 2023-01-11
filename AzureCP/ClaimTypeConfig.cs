498da2d2c49dd3040cda28bfa92e00b3f6a7c742
Yvan Duhamel <yvandev@outlook.fr>
2023-01-11 10:08:51 +0100

    get fields from types ClaimTypeConfig, and IdentityClaimTypeConfig if needed

--- AzureCP/ClaimTypeConfig.cs
+++ AzureCP/ClaimTypeConfig.cs
@@ -206,9 +206,14 @@ namespace azurecp
         /// <param name="configToApply"></param>
         internal void ApplyConfiguration(ClaimTypeConfig configToApply)
         {
-            // Copy non-inherited private fields
-            FieldInfo[] fieldsToCopy = this.GetType().GetFields(BindingFlags.NonPublic | BindingFlags.Instance | BindingFlags.DeclaredOnly);
-            foreach (FieldInfo field in fieldsToCopy)
+            // Copy non-inherited private fields of type ClaimTypeConfig, and also IdentityClaimTypeConfig if current object is of this type
+            List<FieldInfo> fieldsToCopyList = new List<FieldInfo>();
+            fieldsToCopyList.AddRange(typeof(ClaimTypeConfig).GetFields(BindingFlags.NonPublic | BindingFlags.Instance | BindingFlags.DeclaredOnly));
+            if (this is IdentityClaimTypeConfig == true)
+            {
+                fieldsToCopyList.AddRange(this.GetType().GetFields(BindingFlags.NonPublic | BindingFlags.Instance | BindingFlags.DeclaredOnly));
+            }
+            foreach (FieldInfo field in fieldsToCopyList)
             {
                 field.SetValue(this, field.GetValue(configToApply));
             }
