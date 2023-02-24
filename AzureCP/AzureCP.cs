7d2e33ac143d51dbf4cc446482a7dea3a02a7882
Andi Krüger <15608729+andikrueger@users.noreply.github.com>
2023-02-24 11:14:03 +0100

    Added formsrole to FillEntityTypes

--- AzureCP/AzureCP.cs
+++ AzureCP/AzureCP.cs
@@ -918,6 +918,7 @@ namespace azurecp
         protected override void FillEntityTypes(List<string> entityTypes)
         {
             entityTypes.Add(SPClaimEntityTypes.User);
+            entityTypes.Add(SPClaimEntityTypes.FormsRole);
             entityTypes.Add(ClaimsProviderConstants.GroupClaimEntityType);
         }
 
