3a729d209835ceef87ab91da1d368fb982c469c1
Yvan Duhamel <yvandev@outlook.fr>
2023-12-08 14:36:42 +0100

    Fix "Edit" links fail to work on the "Claim types configuration" page (#220)
    * Update ClaimTypesConfig.ascx.cs
    
    * Update CHANGELOG.md

--- Yvand.EntraCP/TEMPLATE/ADMIN/EntraCP/ClaimTypesConfig.ascx.cs
+++ Yvand.EntraCP/TEMPLATE/ADMIN/EntraCP/ClaimTypesConfig.ascx.cs
@@ -30,8 +30,8 @@ namespace Yvand.EntraClaimsProvider.Administration
         readonly string HtmlCellPrefixToBypassLookup = "<span name=\"span_PrefixToBypassLookup_{1}\" id=\"span_PrefixToBypassLookup_{1}\">{0}</span><input name=\"input_PrefixToBypassLookup_{1}\" id=\"input_PrefixToBypassLookup_{1}\" style=\"display:none;\" value=\"{0}\"></input>";
         readonly string HtmlCellDirectoryObjectType = "<span name=\"span_ClaimEntityType_{1}\" id=\"span_ClaimEntityType_{1}\">{0}</span><select name=\"list_ClaimEntityType_{1}\" id=\"list_ClaimEntityType_{1}\" style=\"display:none;\">{2}</select>";
 
-        readonly string HtmlEditLink = "<a name=\"editLink_{0}\" id=\"editLink_{0}\" href=\"javascript:Azurecp.ClaimsTablePage.EditItem('{0}')\">Edit</a>";
-        readonly string HtmlCancelEditLink = "<a name=\"cancelLink_{0}\" id=\"cancelLink_{0}\" href=\"javascript:Azurecp.ClaimsTablePage.CancelEditItem('{0}')\" style=\"display:none;\">Cancel</a>";
+        readonly string HtmlEditLink = "<a name=\"editLink_{0}\" id=\"editLink_{0}\" href=\"javascript:Entracp.ClaimsTablePage.EditItem('{0}')\">Edit</a>";
+        readonly string HtmlCancelEditLink = "<a name=\"cancelLink_{0}\" id=\"cancelLink_{0}\" href=\"javascript:Entracp.ClaimsTablePage.CancelEditItem('{0}')\" style=\"display:none;\">Cancel</a>";
 
         protected void Page_Load(object sender, EventArgs e)
         {
