0a42ac916a54e794e105397fd22a4b99cfae5640
Yvan Duhamel <yvandev@outlook.fr>
2025-07-16 10:45:59 +0200

    Update FilterAccountEnabledTests.cs

--- Yvand.EntraCP.Tests/FilterAccountEnabledTests.cs
+++ Yvand.EntraCP.Tests/FilterAccountEnabledTests.cs
@@ -33,5 +33,18 @@ namespace Yvand.EntraClaimsProvider.Tests
             base.TestSearchAndValidateForTestUser(user);
             base.TestAugmentationAgainst1RandomGroup(user);
         }
+
+        [Test, TestCaseSource(typeof(TestEntitySourceManager), nameof(TestEntitySourceManager.GetSomeGroups), new object[] { TestEntitySourceManager.MaxNumberOfGroupsToTest, true })]
+        public void TestGroups(TestGroup group)
+        {
+            TestSearchAndValidateForTestGroup(group);
+        }
+
+        [Test]
+        public void TestAGuestUser()
+        {
+            TestUser user = TestEntitySourceManager.GetOneUser(UserType.Guest);
+            base.TestSearchAndValidateForTestUser(user);
+        }
     }
 }
